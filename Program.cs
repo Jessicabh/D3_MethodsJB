10eef2bf078a80e25d9a7e7f2b6a37c398363053
Jessica Beckford <jessicabeckf@usf.edu>
2021-02-05 21:49:42 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Author: Jessica Beckford 
+ * Date 2/4/2021
+ */ 
+using System;
+
+namespace D3_MethodsJB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name");
+
+            try 
+            {
+                String input = Console.ReadLine();
+              
+                String greeting_student = Greeting(input);
+            }
+            catch
+            {
+                Console.WriteLine("Press any key to exit the program");
+                Console.ReadKey(true);
+            }
+            
+        } // End of main method 
+
+        private static string Greeting(string input)
+        {
+            String name = input;
+            Console.WriteLine("Hello " + name + "!!");
+            return name;
+        }
+    }
+}
